fb977f14a8cc486311642020cf44b2118557bd40
Faun <woecifaun@gmail.com>
2016-02-28 08:52:57 +0100

    Fix Markdown meta detection regex to include closing ---

--- src/Model/Data/Handler/MarkdownHandler.php
+++ src/Model/Data/Handler/MarkdownHandler.php
@@ -7,7 +7,7 @@
 
 namespace Swag\Model\Data\Handler;
 
-use \Michelf\MarkdownExtra;
+use Michelf\MarkdownExtra;
 use Swag\Model\Data\Data;
 use Symfony\Component\Yaml\Yaml;
 
@@ -25,7 +25,7 @@ class MarkdownHandler extends AbstractDataHandler
         $data     = new Data();
         $start    = 0;
         $contents = file_get_contents($this->node);
-        preg_match('/^[-]{3}(.+?)(?=\n---\n)/s', $contents, $matches);
+        preg_match('/^[-]{3}(.+?)(?:\n---\n)/s', $contents, $matches);
         if (isset($matches[1])) {
             $start = strlen($matches[0]);
             $metas = Yaml::parse($matches[1]);
