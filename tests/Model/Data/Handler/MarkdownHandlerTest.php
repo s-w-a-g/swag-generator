75a9145185d25fd8825ef0e066a675a2cca8242e
Julien Baron <woecifaun@gmail.com>
2016-03-08 13:26:58 +0100

    Add Markdown DataHandler unit tests

---
+++ tests/Model/Data/Handler/MarkdownHandlerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * @Author: Woecifaun
+ * @License: view License file if any
+ */
+namespace Swag\Test\Model\Data\Handler;
+
+use Swag\Model\Data\Data;
+use Swag\Model\Data\Handler\MarkdownHandler;
+
+class MarkdownHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testApply()
+    {
+        $handler = new MarkdownHandler();
+
+        $fileNames = [
+            'test.md'   => true,
+            'test.mdown'  => true,
+            'test.other' => false,
+        ];
+
+        foreach ($fileNames as $fileName => $bool) {
+            $file = new \SplFileInfo($fileName);
+            $this->assertEquals($handler->apply($file), $bool);
+        }
+    }
+
+    public function testSetContent()
+    {
+        $md = __DIR__.'/../../../fixtures/Data/swag.markdown';
+        $file = new \SplFileInfo($md);
+
+        $handler = new MarkdownHandler();
+        $data = $handler->getValue($file);
+
+        $this->assertEquals($data->test, 'php unit');
+        $this->assertEquals($data->tree, ['entry1' => 'one', 'entry2' => 'two']);
+
+        $this->assertEquals((string) $data, "<h1>Hello</h1>\n\n<p>I am some content</p>\n");
+    }
+}
---
+++ tests/fixtures/Data/swag.markdown
@@ -0,0 +1,10 @@
+---
+test: php unit
+tree:
+  entry1: one
+  entry2: two
+---
+
+# Hello
+
+I am some content
