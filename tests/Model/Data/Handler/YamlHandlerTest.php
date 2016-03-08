6946acf54e5a99eff6c62df2eb037c5506c2dd0c
Julien Baron <woecifaun@gmail.com>
2016-03-08 13:16:56 +0100

    Test method apply for  Yaml DataHandler

--- tests/Model/Data/Handler/YamlHandlerTest.php
+++ tests/Model/Data/Handler/YamlHandlerTest.php
@@ -4,19 +4,34 @@
  * @Author: Woecifaun
  * @License: view License file if any
  */
+namespace Swag\Test\Model\Data\Handler;
 
 use Swag\Model\Data\Handler\YamlHandler;
 
-class YamlHandlerTest extends PHPUnit_Framework_TestCase
+class YamlHandlerTest extends \PHPUnit_Framework_TestCase
 {
+    public function testApply()
+    {
+        $handler = new YamlHandler();
+
+        $fileNames = [
+            'test.yml'   => true,
+            'test.yaml'  => true,
+            'test.other' => false,
+        ];
+
+        foreach ($fileNames as $fileName => $bool) {
+            $file = new \SplFileInfo($fileName);
+            $this->assertEquals($handler->apply($file), $bool);
+        }
+    }
+
     public function testSetContent()
     {
         $yml = __DIR__.'/../../../fixtures/Data/swag.yml';
         $file = new \SplFileInfo($yml);
 
         $handler = new YamlHandler();
-
-        $this->assertTrue($handler->apply($file));
 
         $result = [
             'test' => 'php unit',
