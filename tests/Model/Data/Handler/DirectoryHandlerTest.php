b12e46b7601aef5a4e1e69781c34481fa2a3eb19
Faun <woecifaun@gmail.com>
2016-03-10 23:23:41 +0100

    Complete data DirectoryHandler unit tests

--- tests/Model/Data/Handler/DirectoryHandlerTest.php
+++ tests/Model/Data/Handler/DirectoryHandlerTest.php
@@ -9,7 +9,9 @@ namespace Swag\Test\Model\Data\Handler;
 use Swag\Model\Data\DataBuilder;
 use Swag\Model\Data\Handler\DirectoryHandler;
 use Swag\Model\Data\Handler\YamlHandler;
+use Swag\Service\Notifier;
 use Swag\Test\TestCaseTrait;
+use Symfony\Component\Console\Output\StreamOutput;
 
 class DirectoryHandlerTest extends \PHPUnit_Framework_TestCase
 {
@@ -49,7 +51,7 @@ class DirectoryHandlerTest extends \PHPUnit_Framework_TestCase
     /**
      * @expectedException Swag\Model\Data\Exception\InvalidStructureException
      */
-    public function testDuplicateKeys($value='')
+    public function testDuplicateKeys()
     {
         $dir = $this->getFixturesDir().'data';
         $dataBuilder = $this->getDataBuilder();
@@ -58,5 +60,22 @@ class DirectoryHandlerTest extends \PHPUnit_Framework_TestCase
         $dataBuilder->addDataHandler($dirHandler);
 
         $data = $dirHandler->getValue(new \SplFileInfo($dir));
+    }
+
+    public function testSkipInvalidFiles()
+    {
+        $dir         = new \SplFileInfo($this->getFixturesDir().'data/test-dir');
+        $stream      = fopen('php://memory', 'a', false);
+        $output      = new StreamOutput($stream);
+        $notifier    = new Notifier($output);
+        $dataBuilder = new DataBuilder($dir, $notifier);
+        $dirHandler  = new DirectoryHandler();
+        $dataBuilder->addDataHandler($dirHandler);
+
+        $dirHandler->getValue(new \SplFileInfo($dir));
+
+        rewind($output->getStream());
+        $expected = 'Skipping invalid file: '.$dir.'/swag.yml'.PHP_EOL.PHP_EOL;
+        $this->assertEquals($expected, stream_get_contents($output->getStream()));
     }
 }
